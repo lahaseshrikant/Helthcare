b0c74ce85f623c5b57ba29b7e9d6d230025c5340
Shrikant Lahase <138747494+lahaseshrikant@users.noreply.github.com>
2023-12-10 22:50:14 +0530

    New commit

--- public/script.js
+++ public/script.js
@@ -80,7 +80,7 @@ window.onload = function () {
 
 async function fetchConditionDetails(symptoms) {
     const apiUrl = 'http://localhost:3000/login';
-
+  
     const response = await fetch(apiUrl, {
         method: 'POST',
         headers: {
@@ -96,28 +96,5 @@ async function fetchConditionDetails(symptoms) {
         suggestedConditions: data.suggestedConditions,
         inputSymptoms: symptoms,
     };
-}
-
-function myFunction1() {
-    console.log("Function called");
-    var navbar = document.getElementById("navbar");
-    var isNavbarResponsive = navbar.classList.contains("responsive");
-
-    if (!isNavbarResponsive) {
-        navbar.classList.add("responsive");
-        document.addEventListener("click", closeNavbarOnClickOutside);
-    } else {
-        navbar.classList.remove("responsive");
-        document.removeEventListener("click", closeNavbarOnClickOutside);
-    }
-}
-
-function closeNavbarOnClickOutside(event) {
-    var navbar = document.getElementById("navbar");
-    var icon = document.getElementById("navbarButton");
-
-    if (!navbar.contains(event.target) && event.target !== icon) {
-        navbar.className = "navbar";
-        document.removeEventListener("click", closeNavbarOnClickOutside);
-    }
-}
+  }
+  
